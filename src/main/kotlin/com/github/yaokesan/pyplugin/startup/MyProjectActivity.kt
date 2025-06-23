d4de9fc09e678c9617dc48791ac30a8abd9b9413
GitHub Action <action@github.com>
2025-06-23 08:47:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/yaokesan/pyplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yaokesan/pyplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yaokesan.pyplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
